54bb49fdf929cd3a1135cba20e80f9bccf85e54e
Igor Kovalchuk <igorko76@gmail.com>
2019-06-23 23:27:38 +0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # test-02-file-formats-conversion
+
+How to use: Main.java 
+
+To support other file formats, please create your own DocumentProcessor implementation
+and register it.
+
+ConversionAPI.registerProcessor(DocumentProcessor processor);
+
+This library has two predefined formats: 
+ 
+ * ConversionAPI.FORMAT_XML
+ * ConversionAPI.FORMAT_BINARY
+
+Other formats you can define on your own.
+
+Auxiliary classes: DocumentImpl, CarImpl, FileUtils
--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.io.IOException;
 import java.util.Date;
 
+import org.example.conversion.Car;
 import org.example.conversion.ConversionAPI;
 import org.example.conversion.Document;
 
@@ -18,6 +19,15 @@ public class Main {
 		d.remove(2);
 		d.remove(1);
 		System.out.println(d.toString());
+
+		Car c1 = d.get(0);
+		System.out.println("Brand name: " + c1.getBrandName());
+
+		Car c2 = Car.newInstance();
+		c2.setBrandName("Some name");
+		c2.setDate(new Date());
+		c2.setPrice(1000);
+		System.out.println("Selected car: " + c2);
 
 		ConversionAPI.save(ConversionAPI.FORMAT_XML, d, "fixtures/out.xml");
 	}
